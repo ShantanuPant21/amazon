be8f1df1735e989933d0c2ea95b3d04e6aee9338
ShantanuPant21 <156995254+ShantanuPant21@users.noreply.github.com>
2024-01-18 15:48:22 +0530

    added index.html
    added script.js added style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+let a=document.getElementsByClassName("nav-search")[0]
+a.addEventListener("click",function(){
+    a.style.border="2.5px solid orange"
+})
+
+document.body.addEventListener("mouseover",function(){
+    a.style.border="none"
+})
+
